ec8cef59f03b9b9c329a274bf6023d7537f85bec
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2017-05-23 15:08:06 +0200

    added download_url to setup.py for pip installing, __init__.py, README to rst format for PyPi

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='yamlord',
       version='0.1',
       description='wrapper for yaml package to read/write OrderedDict',
-      url='https://github.com/ryanjdillon/yamlord',
       author='Ryan J. Dillon',
       author_email='ryanjamesdillon@gmail.com',
+      url='https://github.com/ryanjdillon/yamlord',
+      download_url='https://github.com/ryanjdillon/yamlord/archive/0.1.tar.gz',
       license='MIT',
       packages=['yamlord'],
       zip_safe=False)
