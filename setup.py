fba8b1238cdffa7c156d4db5ace813f1cb5fc02d
Ryan J. Dillon <ryanjamesdillon@gmail.com>
2017-07-07 15:35:25 +0200

    updated setup.py to include missing pyaml dependency

--- setup.py
+++ setup.py
@@ -8,16 +8,17 @@ with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='yamlord',
-    version='0.2',
+    version='0.3',
     description='wrapper for yaml package to read/write OrderedDict',
     long_description=long_description,
     author='Ryan J. Dillon',
     author_email='ryanjamesdillon@gmail.com',
     url='https://github.com/ryanjdillon/yamlord',
-    download_url='https://github.com/ryanjdillon/yamlord/archive/0.2.tar.gz',
+    download_url='https://github.com/ryanjdillon/yamlord/archive/0.3.tar.gz',
     license='MIT',
     packages=['yamlord'],
     keywords=['python-3'],
+    install_requires=['pyaml'],
     classifiers=[
     'Development Status :: 3 - Alpha',
     'Programming Language :: Python :: 3.5'],
